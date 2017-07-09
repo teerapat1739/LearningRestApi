74f9381b430e1e8e98bc492ff5b9ec284508dbe6
teerapat <teerapat_12345678@hotmail.com>
2017-07-09 22:50:56 +0700

    GeoJSON

--- model/ninja.js
+++ model/ninja.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+// create geolocation Schemac
+const GeoSchema = new Schema({
+  type:{
+    type:String,
+    default:"Point"
+  },
+  coordinates:{
+    type:[Number],
+    index:"2dsphere"
+  }
+});
 // create ninja Schema & model
 const NinjaSchema = new Schema({
     name: {
@@ -13,7 +25,8 @@ const NinjaSchema = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
+    },
+    geometry: GeoSchema
     // add in geo location
 });
 

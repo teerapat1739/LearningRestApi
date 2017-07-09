b507207969bdcf0a469a2791d74fb902485aa3cb
teerapat <teerapat_12345678@hotmail.com>
2017-07-09 18:27:41 +0700

    Error Handling

--- routes/api.js
+++ routes/api.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const Ninja = require('../model/ninja');
 
 //get a list of ninjas from the db
-router.get('/ninjas',function(req,res){
+router.get('/ninjas',function(req,res,next){
     res.send({type:'GET'});
 });
 
 //add a new nonja to the db
-router.post('/ninjas',function(req,res){
+router.post('/ninjas',function(req,res,next){
     Ninja.create(req.body).then(function(ninja){
       res.send(ninja);
-    });
+    }).catch(next);
 });
 
 //update a ninja in the db
-router.put('/ninjas:id',function(req,res){
+router.put('/ninjas:id',function(req,res,next){
     res.send({type:'PUT'});
 });
 
 //delete a ninjas from the db
-router.delete('/ninjas:id',function(req,res){
+router.delete('/ninjas:id',function(req,res,next){
     res.send({type:'DELETE'});
 });
 module.exports = router;
